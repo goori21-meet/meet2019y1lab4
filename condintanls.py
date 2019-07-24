e3ccd92de3c93439253075952f275150122b78ec
goori21-meet <goori21@meet.mit.edu>
2019-07-24 10:00:17 -0400

    Stopping here

---
+++ condintanls.py
@@ -0,0 +1,14 @@
+fruit= input ("what fruit is it?")
+if fruit=="apples":
+    print("apples in bin 1")
+
+
+elif fruit=="bananas":
+    print("bananas in bin 2")
+
+elif fruit=="oranges":
+    print("oranges in bin 3")
+
+else:
+    print ("error cant recognize this fruit")
+          
