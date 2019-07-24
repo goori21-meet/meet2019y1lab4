2dadb23170ce78805a54bd85e5bb88b21c3319b4
goori21-meet <goori21@meet.mit.edu>
2019-07-24 03:27:41 -0400

    Stopping here

---
+++ copycat.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.bgcolor("purple")
+t1=turtle.Turtle()
+t2=turtle.Turtle()
+t1.color("yellow")
+t1.shape('circle')
+t1.goto(0,150)
+t1.goto(-75,150)
+t1.color('white')
+t1.goto(-75,0)
+t2.pensize(10)
+t2.color('green')
+t2.shape('arrow')
+t2.goto(0,-150)
+t2.goto(75,-150)
+t2.color('blue')
+t2.goto(75,0)
+t2.left(90)
+
+
+
+turtle.mainloop
+
+
+         
